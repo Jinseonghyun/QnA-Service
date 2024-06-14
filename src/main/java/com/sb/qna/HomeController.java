eecbe1ac40db01e294dc6ef25a4a96e596021f69
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-14 15:06:31 +0900

    구구단 구현

--- src/main/java/com/sb/qna/HomeController.java
+++ src/main/java/com/sb/qna/HomeController.java
@@ -3,6 +3,9 @@ package com.sb.qna;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 // @Controller : 스프링부트한테 해당 클래스는 컬트롤러 역할이라고 알려준다.
 @Controller
 public class HomeController {
@@ -67,5 +70,51 @@ public class HomeController {
     public int showIncrease() {
         increaseNo++;
         return increaseNo;
+    }
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    // Integer 는 객체로 null 값을 허용한다.
+    public String showGugudan(Integer dan, Integer limit) {
+        if(dan == null) {
+            dan = 9;
+        }
+
+        if (limit == null) {
+            limit = 9;
+        }
+
+        String gugudanFormat = "";
+
+        for(int i = 1; i <= 9; i++) {
+            gugudanFormat += "%d * %d = %d<br>".formatted(dan, i, dan * i);
+        }
+
+        return gugudanFormat;
+    }
+
+    @GetMapping("/gugudan2")
+    @ResponseBody
+    // Integer 는 객체로 null 값을 허용한다.
+    public String showGugudan2(Integer dan, Integer limit) {
+        if(dan == null) {
+            dan = 9;
+        }
+
+        if (limit == null) {
+            limit = 9;
+        }
+
+        String gugudanFormat = "";
+
+        for(int i = 1; i <= 9; i++) {
+            gugudanFormat += "%d * %d = %d<br>".formatted(dan, i, dan * i);
+        }
+
+        // final 수식어가 붙으면 해당 변수는 상수처리 된다.
+        final Integer finalDan = dan;
+        return IntStream.rangeClosed(1, limit)
+                .mapToObj(i-> "%d * %d = %d".formatted(finalDan, i, finalDan * i))
+                .collect(Collectors.joining("<br>"));
     }
 }
