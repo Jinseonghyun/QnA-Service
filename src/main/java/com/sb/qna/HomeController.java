f92dd960b7e4440f84405f4ea1aa6462b2326115
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-17 15:51:14 +0900

    action에서 자동으로 조립된 객체를 입력 받기

--- src/main/java/com/sb/qna/HomeController.java
+++ src/main/java/com/sb/qna/HomeController.java
@@ -386,6 +386,20 @@ public class HomeController {
 
         return "%d번 게시물을 삭제하였습니다.".formatted(id);
     }
+
+    @GetMapping("/addPersonOldWay")
+    @ResponseBody
+    public Person addPersonOldWay(int id, int age, String name) {
+        Person p = new Person(id, age, name);
+
+        return p;
+    }
+
+    @GetMapping("/addPerson")
+    @ResponseBody
+    public Person addPerson(Person p) {
+        return p;
+    }
 }
 
 class Animal {
@@ -464,4 +478,10 @@ class Article {
 //    }
 }
 
-
+@AllArgsConstructor
+@Getter
+class Person {
+    private int id;
+    private int age;
+    private String name;
+}
