77ea426cf981b516dc0a8a6f8e67d1b40b4f4603
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-17 11:37:05 +0900

    다양한 리턴타입 메서드 생성

--- src/main/java/com/sb/qna/HomeController.java
+++ src/main/java/com/sb/qna/HomeController.java
@@ -3,10 +3,15 @@ package com.sb.qna;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -166,4 +171,188 @@ public class HomeController {
 
         return "세션변수의 %s의 값이 %s입니다.".formatted(name, value);
     }
+
+    @GetMapping("/home/returnBoolean")
+    @ResponseBody
+    public boolean showReturnBoolean() {
+        return false;
+    }
+
+    @GetMapping("/home/returnInArr")
+    @ResponseBody
+    public int[] showreturnInArr() {
+        int[] arr = new int[] {10, 20, 30};
+        return arr;
+    }
+
+    @GetMapping("/home/returnStringList")
+    @ResponseBody
+    public List<String> showreturnStringList() {{
+        List<String> list = new ArrayList<>();
+        list.add("안녕");
+        list.add("반가워");
+        list.add("어서와");
+/*
+// 위와 같은 코드
+        List<String> list2 = new ArrayList<>();
+        list.add("안녕");
+        list.add("반가워");
+        list.add("어서와");
+*/
+        return list;
+    }};
+
+    @GetMapping("/home/returnMap")
+    @ResponseBody
+    public Map<String, Object> showreturnMap() {
+        Map<String, Object> map = new LinkedHashMap<>() {{
+            put("id", 1);
+            put("age", 5);
+            put("name", "푸바오");
+
+            put("related", new ArrayList<>() {{
+                add(2);
+                add(3);
+                add(4);
+            }});
+        }};
+
+        return map;
+    }
+
+    @GetMapping("/home/returnAnimal")
+    @ResponseBody
+    public Animal showreturnAnimal() {
+        Animal animal = new Animal(1, 3, "포비", new ArrayList<>() {{
+            add(2);
+            add(3);
+            add(4);
+        }});
+
+        System.out.println("animal = " + animal);
+        return animal;
+    }
+
+    @GetMapping("/home/returnAnimal2")
+    @ResponseBody
+    public AnimalV2 showreturnAnimal2() {
+        AnimalV2 animal = new AnimalV2(1, 3, "포비", new ArrayList<>() {{
+            add(2);
+            add(3);
+            add(4);
+        }});
+        animal.setName(animal.getName() + "V2");
+
+        return animal;
+    }
+
+    @GetMapping("/home/returnAnimalMapList")
+    @ResponseBody
+    public List<Map<String, Object>> showreturnAnimalMapList() {
+
+        Map<String, Object> animalMap1 = new LinkedHashMap<>() {{
+            put("id", 1);
+            put("age", 5);
+            put("name", "푸바오");
+
+            put("related", new ArrayList<>() {{
+                add(2);
+                add(3);
+                add(4);
+            }});
+        }};
+
+        Map<String, Object> animalMap2 = new LinkedHashMap<>() {{
+            put("id", 2);
+            put("age", 8);
+            put("name", "포비");
+
+            put("related", new ArrayList<>() {{
+                add(5);
+                add(6);
+                add(7);
+            }});
+        }};
+
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        list.add(animalMap1);
+        list.add(animalMap2);
+
+        return list;
+    }
+
+    @GetMapping("/home/returnAnimalList")
+    @ResponseBody
+    public List<AnimalV2> showreturnnimalList() {
+        AnimalV2 animal1 = new AnimalV2(1, 3, "포비", new ArrayList<>() {{
+            add(2);
+            add(3);
+            add(4);
+        }});
+
+        AnimalV2 animal2 = new AnimalV2(2, 6, "푸바오", new ArrayList<>() {{
+            add(5);
+            add(6);
+            add(7);
+        }});
+
+        List<AnimalV2> list = new ArrayList<>();
+        list.add(animal1);
+        list.add(animal2);
+
+        return list;
+    }
 }
+
+class Animal {
+    private final int id;
+    private final int age;
+    private final String name;
+    private final List<Integer> related;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Integer> getRelated() {
+        return related;
+    }
+
+    public Animal(int id, int age, String name, List<Integer> related) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.related = related;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", related=" + related +
+                '}';
+    }
+}
+
+@AllArgsConstructor // 생성자 생성
+//@NoArgsConstructor  // 비어 있는 생성자 생성 우리는 final 이기에 생성자 있어야함
+@Data
+class AnimalV2 {
+    private final int id;
+    private final int age;
+    private  String name;
+    private final List<Integer> related;
+
+}
+
