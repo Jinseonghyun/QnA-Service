2677419ac879a242003580b01fac816a3449dba3
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-18 17:06:17 +0900

    findAll

--- src/test/java/com/sb/qna/SbbApplicationTests.java
+++ src/test/java/com/sb/qna/SbbApplicationTests.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class SbbApplicationTests {
@@ -30,6 +34,19 @@ public class SbbApplicationTests {
         q2.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q2);  // 두번째 질문 저장
 
+        // id는 최소 0번보다 크다.
+        assertThat(q1.getId()).isGreaterThan(0);
+        // 두 번째 id는 첫 번째 id보다 크다.
+        assertThat(q2.getId()).isGreaterThan(q1.getId());
+    }
 
+    @Test
+    void testJpa2() {
+        // findAll() : SELECT * FROM question;
+        List<Question> all = questionRepository.findAll();
+        assertEquals(2, all.size());
+
+        Question q = all.get(0); // 위에서 받은 것 중 첫 번째 받은거
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
 }
