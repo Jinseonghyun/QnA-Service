57efda41ebf4361bfe5c52345b8421b8462c5113
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-18 16:15:36 +0900

    레포지토리 추가 후 테스트케이스

--- src/main/java/com/sb/qna/Question.java
+++ src/main/java/com/sb/qna/Question.java
@@ -1,10 +1,14 @@
 package com.sb.qna;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity // 아래 Question 클래스는 엔티티 클래스이다.
 // 아래 클래스와 1:1 로 매칭되는 테이블이 DB에 없다면, 자동으로 생성되어야 한다.
 public class Question {
